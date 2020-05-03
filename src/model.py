89fef48adc4e9440da454424a51e5b3917a1d04c
Danny Xuan Huynh <dxhuynh@uh.edu>
2020-05-02 21:20:28 -0500

    Update model.py

--- src/model.py
+++ src/model.py
@@ -8,7 +8,18 @@ import csv
 
 
 # X-Axis 
-#Input X1 - Avg Temperature 2 weeks before ; Input X2 - Population density
+#Input X1 - Avg Temperature 2 weeks before ; Input X2 - Population density 
+
+popDensitySize = 49*3
+
+popDensityLA = []
+popDensitySnohomish = []
+popDensityNYC = []
+
+for range(popDensitySize) 
+    popDensityLA.append(7554)
+    popDensityNYC.append(26403)
+    popDensitySnohomish.append(2740)
 
 #Y - Axis
 
@@ -17,10 +28,12 @@ import csv
 //Neural Network model for linear regression
 
 model = Sequential()
-model.add(Dense(3, input_dim = 3, activation = 'linear'))
+model.add(Dense(3, input_dim = 2, activation = 'linear'))
 model.add(Dense(3, activation = 'linear'))
 model.add(Dense(1, activation = 'linear'))
 
 model.compile(loss = 'mean squared error', optimizer = 'adam', metrics = 'accuracy')
+
+model.fit(
 
 
